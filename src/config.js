246eb5e71c1d2dc54d06a784e06b5e863a998371
greghab <fxkrait@disroot.org>
2020-01-26 11:38:43 -0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export default {
+    s3: {
+        REGION: "us-west-2",BUCKET: "notes-svless-upload"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_B09tSe4qJ",
+        APP_CLIENT_ID: "4613qjlugemt6fikgd1vbmvvfl",
+        IDENTITY_POOL_ID: "us-west-2:08cf768c-97eb-4e98-9c44-ad54eda88354"
+    }
+};
